d735e46cc8faeadc9f4398a25d10738b16308407
k232629 <olhakaraiman@gmail.com>
2018-02-25 20:39:33 +0200

    Structure changed

--- src/main/java/data/StudentDao.java
+++
@@ -1,8 +0,0 @@
-package main.java.data;
-
-import main.java.Student;
-
-public interface StudentDao {
-    void addStudent(Student student);
-    Student getStudentByIdFromDb(int i);
-}
--- src/main/java/data/TeachersDao.java
+++
@@ -1,9 +0,0 @@
-package main.java.data;
-import main.java.Teacher;
-
-public interface TeachersDao {
-    void addTeacher(Teacher teacher);
-    Teacher getTeacherById(int id);
-    void saveTacher(Teacher teacher);
-
-}
